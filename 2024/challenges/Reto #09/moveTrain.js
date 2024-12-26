2b2727c0c61da01737f4ae4573f64e2062680dfb
AdrianGerman <adrian.german1019@gmail.com>
2024-12-26 08:42:10 -0600

    example

--- 2024/challenges/Reto #09/moveTrain.js
+++ 2024/challenges/Reto #09/moveTrain.js
@@ -1,3 +1,4 @@
+// --- code
 function moveTrain(board, mov) {
   const moves = { L: [0, -1], R: [0, 1], U: [-1, 0], D: [1, 0] }
   let headRow, headCol
@@ -25,3 +26,19 @@ function moveTrain(board, mov) {
   if (nextCell === "*") return "eat"
   return "none"
 }
+// ---
+
+// example
+const board = ["·····", "*····", "@····", "o····", "o····"]
+
+console.log(moveTrain(board, "U"))
+// ➞ 'eat'
+
+console.log(moveTrain(board, "D"))
+// ➞ 'crash'
+
+console.log(moveTrain(board, "L"))
+// ➞ 'crash'
+
+console.log(moveTrain(board, "R"))
+// ➞ 'none'
